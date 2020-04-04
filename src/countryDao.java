86559d9af9615b2427b5b90ffaf43ad4e39cfc13
Akyra88 <tiagosilva_akyra88@hotmail.com>
2020-04-04 11:44:15 +0100

    Update countryDao.java
    just adding some comments.

--- src/countryDao.java
+++ src/countryDao.java
@@ -1,9 +1,10 @@
 import java.util.List;
 // this is my data access object.
 public interface countryDao {
-	
+	// parameters returning code and name
 	Country findByCode(String code);
 	Country findByName(String name);
+	// simple returning the array list 
 	List <Country> findAll();	
 	void create (Country country);
 }
